fdbda1649c11457c5bd4298a1162840af9e949a2
stanley1208 <stanley.mei08@gmail.com>
2021-09-10 19:19:39 +0800

    java

--- src/main/java/com/mvc/controller/Hello.java
+++ src/main/java/com/mvc/controller/Hello.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +22,17 @@ public class Hello {
 	@ResponseBody
 	public String time() {
 		return new Date().toString();
+	}
+	
+	/*
+	 * 路徑:/sayhi?name=stanley&age=18
+	 * 帶入:name與age兩個參數
+	 * */
+	@RequestMapping(value = "/sayhi")
+	@ResponseBody
+	public String sayHi(@RequestParam("name") String name,
+			@RequestParam("age") Integer age) {
+		
+		return "Hello "+name+", "+age;
 	}
 }
