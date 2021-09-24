ad0eaa96608d2801c9b3eec4456ceda6ea7fc19b
stanley1208 <stanley.mei08@gmail.com>
2021-09-24 20:08:14 +0800

    java

---
+++ src/main/java/com/mvc/entity/products/Group.java
@@ -0,0 +1,33 @@
+package com.mvc.entity.products;
+
+public class Group {
+	private Integer gid;//分類id
+	private String gname;//分類名稱
+	public Group() {
+		super();
+	}
+	public Group(Integer gid, String gname) {
+		super();
+		this.gid = gid;
+		this.gname = gname;
+	}
+	public Integer getGid() {
+		return gid;
+	}
+	public void setGid(Integer gid) {
+		this.gid = gid;
+	}
+	public String getGname() {
+		return gname;
+	}
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	@Override
+	public String toString() {
+		return "Group [gid=" + gid + ", gname=" + gname + "]";
+	}
+	
+	
+	
+}
